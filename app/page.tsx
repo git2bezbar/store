3afce180b39dd12d4113f13720f5d55dee6bc76b
Adem <ademduran6888@gmail.com>
2023-12-16 17:11:43 +0100

    fix(app): adding links for navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import ProductCard from './@products/page';
 import products from '../services/products.json';
 
@@ -25,12 +26,16 @@ export default function Home() {
             })
           }
         </div>
-        <button className="px-4 py-2 w-[300px] bg-black/50 backdrop-blur rounded-lg border border-white/20 duration-300 hover:bg-white/20">Voir tous les produits</button>
+        <Link href="/products">
+          <button className="px-4 py-2 w-[300px] bg-black/50 backdrop-blur rounded-lg border border-white/20 duration-300 hover:bg-white/20">Voir tous les produits</button>
+        </Link>
       </div>
       <div className="flex flex-col gap-16 mb-32 items-center">
         <h2 className="text-6xl font-bold text-center tracking-tight">Notre histoire</h2>
         <p className="max-w-[60%] text-center">Bienvenue dans l&apos;univers de l&apos;AD Store, là où l&apos;art et le shopping se rencontrent pour créer une expérience unique. Notre histoire commence à Bordeaux, une ville vibrante, où nous avons donné vie à l&apos;idée de rendre l&apos;art plus accessible à tous. Inspirés par la diversité artistique de Paris, nous avons élargi notre horizon, collaborant avec des talents passionnés pour enrichir notre collection. Aujourd&apos;hui, l&apos;AD Store ne se contente pas d&apos;être une simple plateforme e-commerce, mais plutôt un voyage artistique qui s&apos;étend au-delà des frontières.</p>
-        <button className="px-4 py-2 w-[300px] bg-black/50 backdrop-blur rounded-lg border border-white/20 duration-300 hover:bg-white/20">En apprendre plus</button>
+        <Link href="/about">
+          <button className="px-4 py-2 w-[300px] bg-black/50 backdrop-blur rounded-lg border border-white/20 duration-300 hover:bg-white/20">En apprendre plus</button>
+        </Link>
       </div>
     </>
   )

0c06c4323caf49bc4cb28744d0eb8d844eeb4e42
Adem <ademduran6888@gmail.com>
2023-12-17 19:07:01 +0100

    feat(legal): adding a custom legal link

---
+++ app/@legal/page.tsx
@@ -0,0 +1,13 @@
+export default function LegalLinks() {
+  return(
+    <div className="flex flex-col gap-4 p-4">
+      <img
+        className="rounded-lg w-full max-w-[500px]"
+        src="/shhh.png"
+        alt="Shhh"
+        width={500}
+      />
+      <p className="text-center">Rien à voir ici...</p>
+    </div>
+  )
+}
